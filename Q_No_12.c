2bac35352221dd2a7422c1012dd90d223fe9a00e
ghazalrajab <135308993+ghazalrajab@users.noreply.github.com>
2023-06-02 12:50:29 +0500

    Add files via upload

---
+++ Q_No_12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char ch;
+
+    printf("Enter any Character ? ");
+    scanf("%c", &ch);
+
+    printf("ASCII Value of %c character is : %d\n", ch, ch);
+
+    return 0;
+}
+
