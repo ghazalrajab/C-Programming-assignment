37f21433049df9ea73320976396d6a3e9328a29b
ghazalrajab <135308993+ghazalrajab@users.noreply.github.com>
2023-06-02 11:41:04 +0500

    Add files via upload

---
+++ Q_No_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+char name[] = "Raimal Raja";
+char father_name[] = "Ramchand";
+int age = 19;
+char grade = 'A';
+
+printf("Name  \t\t:%s\n", name);
+printf("Father's Name   :%s\n", father_name);
+printf("Age  \t\t:%d years\n", age);
+printf("Grade  \t\t:%c\n", grade);
+
+return 0;
+}
+
